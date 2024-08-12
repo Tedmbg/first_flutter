70bbd6d58946751a065784e26d06c26c7940f24c
tedmbg <tedmbg9@gmail.com>
2024-08-12 19:23:00 +0300

    second  push

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,21 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(
-    MaterialApp(),
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+              child: Text(
+            'Hello world',
+            style: TextStyle(
+              fontSize: 24,
+            ),
+          )),
+          backgroundColor: Color(0xFFc0563f),
+        ),
+        body: Text('Welcome'),
+        backgroundColor: Color(0xFF020643),
+      ),
+    ),
   );
 }
